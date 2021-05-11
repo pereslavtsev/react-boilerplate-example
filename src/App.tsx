3821d68c6377889c245ff7c625b64f623efb4bd5
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2021-05-11 23:37:34 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const App = () => {
+  console.log(33333);
+  return <h1>Hello React</h1>;
+};
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+  },
+  plugins: [new CleanWebpackPlugin()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.tsx', '.ts'],
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          format: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: __dirname + '/public/index.html' }),
+  ],
+};
